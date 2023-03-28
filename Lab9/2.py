20430616690dfb4d1562c142ab628a41cdb726b7
MalikaNurkey <em@em.ru>
2023-03-28 09:22:26 +0600

    'Lab9-mal'

---
+++ Lab9/2.py
@@ -0,0 +1,20 @@
+# Напишите функцию в результате которая возвращает список, кортеж
+# и слоаварь. Пример должен быть индивидуальным.
+
+
+def Foo():
+    list = []
+    dict = {}
+    for i in range(3):
+        name = input('Введите имя: \n')
+        age = input('Введите возраст: \n')
+        list.append(name)
+        dict[name] = age
+    tuple = ('Количество человек в списке', len(list))
+    return list, tuple, dict
+
+list, tuple, dict = Foo()
+
+print(list)
+print(dict)
+print(tuple)
